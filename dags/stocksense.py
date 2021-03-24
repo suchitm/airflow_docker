57607666b62015d2ba18cb260210206e920e5e94
Suchit Mehrotra <suchitmehrotra@gmail.com>
2021-03-24 15:45:15 -0500

    adding updates to stocksense graph

--- dags/stocksense.py
+++ dags/stocksense.py
@@ -4,17 +4,20 @@ import airflow
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.postgres_operator import PostgresOperator
 
 dag = DAG(
     dag_id = "stocksense", 
     start_date = airflow.utils.dates.days_ago(1), 
     schedule_interval = "@hourly", 
+    template_searchpath = "/tmp"
 )
 
 def _get_data(year, month, day, hour, output_path, **_):
    url = (
        "https://dumps.wikimedia.org/other/pageviews/"
-       f"{year}/{year}-{month:0>2}/pageviews-{year}{month:0>2}{day:0>2}-{hour:0>2}0000.gz"
+       f"{year}/{year}-{month:0>2}/pageviews-{year}{month:0>2}{day:0>2}-"
+       f"{hour:0>2}0000.gz"
    )
    print(url)
    request.urlretrieve(url, output_path)
@@ -26,7 +29,8 @@ get_data = PythonOperator(
        "year": "{{ execution_date.year }}",
        "month": "{{ execution_date.month }}",
        "day": "{{ execution_date.day }}",
-       "hour": "{{ execution_date.hour }}",
+       # "hour": "{{ execution_date.hour }}",
+       "hour": 10,
        "output_path": "/tmp/wikipageviews.gz",
    },
    dag=dag,
@@ -38,22 +42,38 @@ extract_gz = BashOperator(
     dag=dag,
 )
  
- 
-def _fetch_pageviews(pagenames):
-   result = dict.fromkeys(pagenames, 0)
-   with open(f"/tmp/wikipageviews", "r") as f:
-       for line in f:
-           domain_code, page_title, view_counts, _ = line.split(" ")
-           if domain_code == "en" and page_title in pagenames:
-               result[page_title] = view_counts
- 
-   print(result)
- 
+def _fetch_pageviews(pagenames, date):
+  result = dict.fromkeys(pagenames, 0)
+  with open("/tmp/wikipageviews", "r", encoding = "utf-8") as f:
+     for line in f:
+         domain_code, page_title, view_counts, _ = line.split(" ")
+         if domain_code == "en" and page_title in pagenames:
+             result[page_title] = view_counts
+
+  with open("/tmp/postgres_query.sql", "w") as f:
+    for pagename, pageviewcount in result.items():
+      f.write(
+        "INSERT INTO pageview_counts VALUES("
+        f"'{pagename}, {pageviewcount}, '{date}'"
+        ");\n"
+      )
+
 fetch_pageviews = PythonOperator(
-    task_id="fetch_pageviews",
-    python_callable=_fetch_pageviews,
-    op_kwargs={"pagenames": {"Google", "Amazon", "Apple", "Microsoft", "Facebok"}},
-    dag=dag,
+  task_id = "fetch_pageviews",
+  python_callable = _fetch_pageviews,
+  op_kwargs = {
+    "pagenames": 
+      {"Google", "Amazon", "Apple", "Microsoft", "Facebook"}, 
+    "date": "{{ execution_date }}"
+  },
+  dag=dag
 )
 
-get_data >> extract_gz >> fetch_pageviews
+write_to_postgres = PostgresOperator(
+  task_id = "write_to_postgres", 
+  postgres_conn_id = "my_postgres", 
+  sql = "postgres_query.sql",
+  dag = dag
+)
+
+get_data >> extract_gz >> fetch_pageviews >> write_to_postgres
